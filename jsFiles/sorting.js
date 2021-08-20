a1e74b1c737cf6adcaff04149e5ce6057fd21cd2
aezakmi007 <abdullahfarhan207@gmail.com>
2021-08-20 11:59:11 +0530

    generated divs fo random height

---
+++ jsFiles/sorting.js
@@ -0,0 +1,18 @@
+let arr = [];
+const generateRandom = () => {
+  return Math.floor(Math.random() * 500);
+};
+const element = document.getElementById("bars");
+const generateBars = () => {
+  element.innerHTML = "";
+  for (let i = 0; i < 100; i++) {
+    arr[i] = generateRandom();
+  }
+  for (let i = 0; i < 100; i++) {
+    const bar = document.createElement("div");
+    console.log(bar);
+    element.appendChild(bar).classList.add("bar");
+    bar.style.height = `${arr[i]}px`;
+  }
+};
+document.getElementById("NA").addEventListener("click", generateBars);
