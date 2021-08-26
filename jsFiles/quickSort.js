c48a8ca7435a787780e6c6759c7505a6c64b08d0
aezakmi007 <abdullahfarhan207@gmail.com>
2021-08-26 10:59:19 +0530

    Quick Sort some bugs left

--- jsFiles/quickSort.js
+++ jsFiles/quickSort.js
@@ -3,38 +3,44 @@ const children = getBars();
 const partition = async function (arr, low, high) {
   console.log("inside partition function");
   let pivot = parseInt(arr[low].style.height);
-  arr[low].style.backgroundColor = "black";
+  arr[low].style.backgroundColor = "red";
   let i = low + 1;
   let j = high;
-  arr[i].style.backgroundColor = "red";
-  arr[j].style.backgroundColor = "red";
+  arr[i].style.backgroundColor = "pink";
+  arr[j].style.backgroundColor = "pink";
   while (true) {
     while (i <= j && parseInt(arr[i].style.height) <= pivot) {
+      await waithere();
+      arr[i].style.backgroundColor = "yellow";
       i++;
-      arr[i].style.backgroundColor = "Red";
-      arr[i - 1].style.backgroundColor = "yellow";
+      if (i <= high && arr[i].style.backgroundColor != "green") {
+        arr[i].style.backgroundColor = "pink";
+      }
     }
 
     while (parseInt(arr[j].style.height) >= pivot && j >= i) {
+      await waithere();
+      arr[j].style.backgroundColor = "yellow";
       j--;
-      arr[j].style.backgroundColor = "Red";
-      arr[j + 1].style.backgroundColor = "yellow";
+      if (j >= low && arr[j].style.backgroundColor != "red") {
+        arr[j].style.backgroundColor = "pink";
+      }
     }
-    // await waithere();
+
     if (j < i) {
-      arr[i].style.backgroundColor = "yellow";
       break;
     } else {
       await waithere();
       swap(arr[i], arr[j]);
-      arr[i].style.backgroundColor = "yellow";
-      arr[j].style.backgroundColor = "yellow";
     }
   }
   await waithere();
   swap(arr[low], arr[j]);
   arr[j].style.backgroundColor = "green";
-  arr[low].style.backgroundColor = "yellow";
+  arr[low].style.backgroundColor = "green";
+  if (i <= high && arr[i].style.backgroundColor != "green") {
+    arr[i].style.backgroundColor = "yellow";
+  }
   return j;
 };
 
